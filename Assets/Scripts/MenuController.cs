adfcdaf4e9dfbc0427e48c0e097b1a9d734b4886
Lucas Fransson <lucas.fransson@hotmail.com>
2024-04-15 02:34:03 +0200

    Start to add Save/Load

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     public Transform[] doNotDestroyOnLoad;
+    public TextMeshProUGUI buttonText;
+
+    public static MenuController instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     void Start()
     {
--- Assets/Scripts/SaveLoadController.cs
+++ Assets/Scripts/SaveLoadController.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveLoadController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameSave gameSave = new GameSave();
+    public static bool hasLoadedFromFile = false;
+
+    public static SaveLoadController instance;
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        LoadGame();
     }
+
+    public static void SaveGame()
+    {
+        if (gameSave != null)
+        {
+            string jsonData = JsonUtility.ToJson(gameSave);
+            PlayerPrefs.SetString("GameSaveData", jsonData);
+            PlayerPrefs.Save();  // Don't forget to save PlayerPrefs changes.
+            Debug.Log("Game saved!");
+        }
+    }
+
+    public static void LoadGame()
+    {
+        string jsonData = PlayerPrefs.GetString("GameSaveData", "{}");
+        gameSave = JsonUtility.FromJson<GameSave>(jsonData);
+        if (gameSave != null)
+        {
+            Debug.Log("Game loaded!");
+            hasLoadedFromFile = true;
+
+            if (SceneManager.GetActiveScene().name == "")
+            {
+                MenuController.instance.buttonText.SetText("Continue");
+            }
+        }
+        else
+        {
+            Debug.Log("No save data found!");
+        }
+            
+    }
+}
+
+[Serializable]
+public class GameSave
+{
+
 }
